cfa787aede752e15dd633daad7bd2cf2f81a53cc
unknown <rodrigosperandio51@gmail.com>
2021-09-23 21:31:04 -0300

    Python Class

---
+++ AulaPython/exercicio1.py
@@ -0,0 +1,7 @@
+print("Calcule a area de um retangulo")
+
+base = input("Qual o tamanho da base do seu retangulo? ")
+altura = input("Qual o tamanho da altura do seu retangulo? ")
+area = float(base) * float(altura)
+
+print(f'O seu retangulo possui area: {area} unidades de metida')
