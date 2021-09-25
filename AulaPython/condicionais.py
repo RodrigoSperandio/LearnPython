3852316746b7c5e99c651537763857a8b95281e2
unknown <rodrigosperandio51@gmail.com>
2021-09-24 22:00:27 -0300

    Condicionais/Exercicio

---
+++ AulaPython/condicionais.py
@@ -0,0 +1,12 @@
+
+
+x = 3
+y = 8
+
+if y > x:
+    print("y eh maior que x")
+else:
+    print("y eh menor ou igual a x")
+
+print("Codigo fora fo bloco condiocinal")
+print(y>x)
--- AulaPython/exercicio2.py
+++ AulaPython/exercicio2.py
@@ -1,1 +1,14 @@
-idade = input("Qual a sua idade? ")
+
+print("Qual a idade da pessoa?")
+
+idade = input("Idade? ")
+idade = int(idade)
+
+if idade < 16:
+    print("Menor de idade")
+elif idade > 18:
+    print("Maior de idade")
+elif idade < 18 > 17:
+    print("Emancipado")
+
+print(f'A pessoa tem a idade de: {idade} anos' )
